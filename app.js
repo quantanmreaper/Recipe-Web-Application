559c9828d92e10f6d0bf7fed738ff5d20346ce53
quantanmreaper <138207080+quantanmreaper@users.noreply.github.com>
2024-10-06 19:59:21 +0300

    categories changes

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ window.onload = function() {
 
     // Start the auto-slide timer
     var currentImg = 0;
-    var interval = 2000;
+    var interval = 2500;
     var timer = setInterval(autoSlide, interval);
 
     function autoSlide() {
@@ -54,7 +54,45 @@ window.onload = function() {
 
     document.addEventListener("DOMContentLoaded", function() {
         var registrationForm = document.getElementById("registrationForm");
-      
+        var reciperegForm = document.getElementById("recipeform");
+        
+        //Validation of recipe form 
+        if(recipeform){
+            recipeform.addEventListener("submit", function(event){
+
+                let isValid = true;
+                document.querySelectorAll('.error').forEach(e => e.remove());
+
+                const recipename = document.getElementById("recipename").value.trim();
+                const ingredients = document.getElementById("ingredients").value.trim();
+                const recipesteps = document.getElementById("recipesteps").value.trim();
+                const recipecategory = document.getElementById("recipecategory").value;
+
+                if(recipename === ""){
+                    showError("Please enter the recipe name.", "recipename");
+                    isValid = false;
+                }
+                if(ingredients === ""){
+                    showError("Please enter the ingredients.", "ingredients");
+                    isValid = false;
+                }
+                if(recipesteps === ""){
+                    showError("Please enter the recipe steps.", "recipesteps");
+                    isValid = false;
+                }
+                if(recipecategory === ""){
+                    showError("Please select a category.", "recipecategory");
+                    isValid = false;
+                }
+                if (!isValid) {
+                    event.preventDefault();
+                }
+
+            });
+        }
+
+
+        // Validate registration form
         if (registrationForm) {
             registrationForm.addEventListener("submit", function(event) {
                 let isValid = true;
